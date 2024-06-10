f0023c3c3d76507ef6997193a60b7f47b5400608
JadAoun1 <jadaoun34@outlook.com>
2024-06-10 01:10:36 -0400

    Changed styling.

--- attendnow/attendance/urls.py
+++ attendnow/attendance/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('signin/', views.sign_in_view, name='signin'),
-    path('signup/', views.sign_up_view, name='signup'),
+    path('sign-up/', views.sign_up_view, name='sign_up'),
+    path('sign-in/', views.sign_in_view, name='sign_in'),
     path('profile/', views.profile_view, name='profile'),
+    path('logout/', views.logout_view, name='logout'),
+    path('about/', views.about, name='about'),
     path('attendance/', views.attendance_view, name='attendance'),
     path('settings/', views.settings_view, name='settings'),
 ]
