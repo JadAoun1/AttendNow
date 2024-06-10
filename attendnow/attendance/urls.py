61ba580592e88cd03ffba4151efaf6f0b0f9a766
JadAoun1 <jadaoun34@outlook.com>
2024-06-10 01:21:01 -0400

    Handles basic user creation/login, navigates to a proffile page when logged in, and creates a new logged in view.

--- attendnow/attendance/urls.py
+++ attendnow/attendance/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('sign-up/', views.sign_up_view, name='sign_up'),
-    path('sign-in/', views.sign_in_view, name='sign_in'),
-    path('profile/', views.profile_view, name='profile'),
-    path('logout/', views.logout_view, name='logout'),
     path('about/', views.about, name='about'),
+    path('sign_in/', views.sign_in_view, name='sign_in'),
+    path('sign_up/', views.sign_up_view, name='sign_up'),
+    path('profile/', views.profile_view, name='profile'),
     path('attendance/', views.attendance_view, name='attendance'),
     path('settings/', views.settings_view, name='settings'),
+    path('logout/', views.logout_view, name='logout'),
 ]
