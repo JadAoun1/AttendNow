f581b5291e3df5315eacf273f332480b4409cfc6
JadAoun1 <jadaoun34@outlook.com>
2024-07-02 16:33:12 -0400

    Changed styling, added more content, created a dashboard page to view and submit attendance.

--- attendnow/attendance/urls.py
+++ attendnow/attendance/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('sign_in/', views.sign_in_view, name='sign_in'),
+    path('', views.home_view, name='home'),
+    path('admin/', admin.site.urls),
+    path('sign_in/', auth_views.LoginView.as_view(template_name='attendance/signIn.html'), name='sign_in'),
     path('sign_up/', views.sign_up_view, name='sign_up'),
     path('profile/', views.profile_view, name='profile'),
     path('attendance/', views.attendance_view, name='attendance'),
@@ -12,6 +16,6 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('submit_attendance/', views.submit_attendance, name='submit_attendance'),
     path('facial_recognition/', views.facial_recognition, name='facial_recognition'),
-
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 
 ]
