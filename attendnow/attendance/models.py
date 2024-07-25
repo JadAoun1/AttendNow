66694e85e9bba9b2c0dbf695422a28234ec0de13
Muhamed El Majzoub <muhamedelmajzoub@hotmail.com>
2024-07-25 03:01:05 +0300

    Added  Major Changes
    Added Admin Panel with Admin Registration and Sign UP
    Get Users API
    GET Weekly Attendance APi with ability to filter by day and user
    Change SMTP Api allowing us to modify from where the email is sent
    
    Fixed Admin sign in flows connecting the backend and the frontend
    Fixed student user flows and html files.
    Added Images Preview for the super admin to be able to see his users

--- attendnow/attendance/models.py
+++ attendnow/attendance/models.py
@@ -27,7 +27,9 @@ class User(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-
+    smtp_password = models.CharField(max_length=100,default=None,null=True,blank=True)
+    smtp_email = models.CharField(max_length=200,default=None,null=True,blank=True)
+    username = models.CharField(max_length=200,default=None,null=True,blank=True)
     objects = UserManager()
 
     USERNAME_FIELD = 'university_id'
