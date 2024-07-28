3693e22df449c630e92d292ff6e89ba486147ef8
JadAoun1 <jadaoun34@outlook.com>
2024-07-28 19:18:09 -0400

    Added svg images to sign in, sign, up, and home. Removed google buttons, reformatted home page.

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\jadao\Desktop\AttendNow-9\venv\Scripts\python.exe
+#!C:\Users\jadao\Desktop\AttendNow-14\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
